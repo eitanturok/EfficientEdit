6f8b312ac6125a29a93971b1b50dad45643b0d97
Eitan Turok <eitan.turok@gmail.com>
2025-10-29 08:46:19 +0000

    testing works

--- data/CanItEdit/test.py
+++ data/CanItEdit/test.py
@@ -1,22 +1,19 @@
-import json
 import subprocess
 import tempfile
 import os
-import re
-from util import (
-    read_json,
-    save_json
-)
+from argparse import ArgumentParser
+
 from tqdm import tqdm
+
+from util import read_json
 
 from icecream import install
 install()
 
 def test_completions(data_dict: dict) -> bool:
     try:
-        completion_code = data_dict["completions"][0]
+        completion_code = data_dict["completions"]
         test_code = data_dict["tests"]
-        ic(completion_code, test_code)
         with tempfile.NamedTemporaryFile(mode="w", suffix=".py", delete=False) as temp_file:
             temp_file.write(completion_code)
             temp_file.write("\n\n")
@@ -41,25 +38,32 @@ def test_completions(data_dict: dict) -> bool:
         if temp_file_path and os.path.exists(temp_file_path):
             os.remove(temp_file_path)
 
+def main(args):
+    test_json = read_json(args.output_path,True)
+    ic(test_json)
+    pass_result = 0
+    # throughput = 0
+    for data in tqdm(test_json):
+        # throughput += data['throughput']
+        if 'pass' in data:
+            if data['pass']:
+                pass_result+=1
+            continue
+        idx = data["completions"].find('``')
+        data["completions"] =  data["completions"][:idx-1] if idx != -1 else data["completions"]
+        ic(data["completions"])
 
-test_path = "result/example.jsonl"
-test_json = read_json(test_path,True)
-pass_result = 0
-throughput = 0
-for data in tqdm(test_json):
-    throughput += data['throughput']
-    if 'pass' in data:
-        if data['pass']:
+        if test_completions(data)==True:
             pass_result+=1
-        continue
-    idx = data["completions"][0].find('``')
-    data["completions"][0] =  data["completions"][0][:idx-1] if idx != -1 else data["completions"][0]
+            data['pass'] = True
+        else:
+            data['pass'] = False
+    print(f"pass@1: {pass_result/len(test_json)}")
+    # print(f"throughput: {throughput/len(test_json)}")
+    ###save_result###
 
-    if test_completions(data)==True:
-        pass_result+=1
-        data['pass'] = True
-    else:
-        data['pass'] = False
-print(f"pass@1: {pass_result/len(test_json)}")
-print(f"throughput: {throughput/len(test_json)}")
-###save_result###
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--output-path", type=str, default="result/example.jsonl")
+    args = parser.parse_args()
+    main(args)
